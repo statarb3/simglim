ffc62febfb5e7e367707633ab1cc14947467cfa9
RforFinance <w.erselina@gmail.com>
2013-01-10 21:47:19 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simglim::Application.config.secret_token = 'd6f30c37e7e97c52ff37e219840354c1157c533ca6e5c2754f79f351f7bd8f92f21c534570b854f40ec529897ec92e6b4d54f75706d624a4426f705d4a6830bc'
